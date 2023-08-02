cfc65044cfc199e44322a64835809f6095a46705
Acexy <thanjava@gmail.com>
2023-08-02 23:05:34 +0800

    定义gorm load

--- gormmodule/gormloader.go
+++ gormmodule/gormloader.go
@@ -2,9 +2,32 @@ package gormmodule
 
 import (
 	"github.com/golang-acexy/starter-parent/parentmodule/declaration"
+	"strconv"
+	"strings"
+)
+
+type DBType string
+
+const (
+	defaultCharset = "utf8mb4"
+)
+const (
+	DBTypeMySQL = "mysql"
 )
 
 type GormModule struct {
+	Username string
+	Password string
+	Host     string
+	Port     uint8
+	Database string
+
+	// default charset : utf8mb4
+	Charset string
+
+	// only mysql now
+	DBType DBType
+
 	GormModuleConfig *declaration.ModuleConfig
 	GormInterceptor  *func(instance interface{})
 }
@@ -34,4 +57,21 @@ func (g *GormModule) Register(interceptor *func(instance interface{})) error {
 
 func (g *GormModule) Unregister(maxWaitSeconds uint) (gracefully bool, err error) {
 	return false, nil
+}
+
+func (g *GormModule) toCoonDsn() string {
+	if g.DBType == "" {
+		g.DBType = DBTypeMySQL
+	}
+	if g.Charset == "" {
+		g.Charset = defaultCharset
+	}
+	var builder strings.Builder
+	switch g.DBType {
+	case DBTypeMySQL:
+		builder.WriteString(g.Username + ":" + g.Password + "@tcp(" + g.Host + ":" + strconv.Itoa(int(g.Port)) + ")/" + g.Database)
+		builder.WriteString("?charset=" + g.Charset)
+		builder.WriteString("&parseTime=True") // support time.Time
+	}
+	return builder.String()
 }
