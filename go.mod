e9d5fd61c0e23f2e5a58e064b0b2c1c931f958ec
Acexy <thankjava@gmail.com>
2023-08-02 20:26:32 +0800

    gorm模块初始化

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/golang-acexy/starter-gorm
+
+go 1.18
+
+require github.com/golang-acexy/starter-parent v0.0.0-20230802064427-5bec9a8fd9a5 // indirect
---
+++ gormmodule/gormloader.go
@@ -0,0 +1,33 @@
+package gormmodule
+
+import (
+	"github.com/golang-acexy/starter-parent/parentmodule/declaration"
+)
+
+type GormModule struct {
+	GormModuleConfig *declaration.ModuleConfig
+	GormInterceptor  *func(instance interface{})
+}
+
+func (g *GormModule) ModuleConfig() *declaration.ModuleConfig {
+	if g.GormModuleConfig != nil {
+		return g.GormModuleConfig
+	}
+	return &declaration.ModuleConfig{
+		ModuleName:               "Gorm",
+		UnregisterPriority:       1,
+		UnregisterAllowAsync:     true,
+		UnregisterMaxWaitSeconds: 20,
+	}
+}
+
+func (g *GormModule) Register(interceptor *func(instance interface{})) error {
+	return nil
+}
+func (g *GormModule) Interceptor() *func(instance interface{}) {
+	return nil
+}
+
+func (g *GormModule) Unregister(maxWaitSeconds uint) (gracefully bool, err error) {
+	return false, nil
+}
